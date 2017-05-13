a8d4ec475df9bf6edb80929a092edeba580dc9ad
JOHN YU <yjunji@10-148-140-34.dhcp-m.deakin.edu.au>
2017-05-13 18:41:40 +1000

    update

--- project/project/ViewController.swift
+++ project/project/ViewController.swift
@@ -10,8 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fitness: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
